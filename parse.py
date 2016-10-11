301b0f42a00cc70c5b222e321be7219c5081db3d
= <rkskek9254@gmail.com>
2016-10-11 21:59:10 +0900

    no thread

--- parse.py
+++ parse.py
@@ -1,13 +1,12 @@
 # -*- coding:utf-8 -*-
 from urllib2 import urlopen, quote
-from multiprocessing import Process, Queue
 import xlrd, xlwt
 import json, os, time, random
 
-def isset( variable ):
+def isset(variable):
 	return variable in locals() or variable in globals()
 
-def get_gps( data ):
+def get_gps(data):
 	data = json.load( urlopen( "https://maps.googleapis.com/maps/api/geocode/json?address=" + data ) )
 	if "OK" != data[ 'status' ]:
 		return 0
@@ -16,20 +15,6 @@ def get_gps( data ):
 		lng = coordinates[ 'geometry' ][ 'location' ][ 'lng' ]
 	return ( lat,lng )
 
-def th_run( wh2, i, j, data ):
-	data = get_gps( quote( str( data ) ) )
-	if data == 0:
-		print "NO DATA"
-		wh2.write( i, j, str( 0 ) )
-		wh2.write( i, j + 1, str( 0 ) )
-	else:
-		#print data
-		print data
-		wh2.write( i, j, str( data[0] ) )
-		wh2.write( i, j + 1, str( data[1] ) )
-	return
-
-s_time = time.time()
 UPLOAD_FOLDER = os.path.dirname( os.path.abspath( __file__ ) )
 filename = "test.xlsx"
 path = UPLOAD_FOLDER
@@ -60,16 +45,18 @@ if geo_col == None:
 wh2.write( 0, col, 'Latitude')
 wh2.write( 0, col+1, 'Longitude')
 
-p = []
-
-for i in range( 1, row ):
+for i in range( 1,row ):
 	for j in range( col ):
 		wh2.write( i, j, wh.cell_value( i, j ) )
 	#print quote( str( wh.cell_value( i,geo_col ) ) )
-	p.append( Process( target=th_run, args=( wh2, i, col, wh.cell_value( i,geo_col ) ) ) )
-	p[ i - 1 ].start()
-for i in range( 1, row ):
-	p[ i - 1 ].join()
+	data = get_gps( quote( str( wh.cell_value( i,geo_col ) ) ) )
+	if data == 0:
+		#print "NO DATA"
+		wh2.write( i, col, str( 0 ) )
+		wh2.write( i, col + 1, str( 0 ) )
+	else:
+		#print data
+		wh2.write( i, col, str( data[0] ) )
+		wh2.write( i, col + 1, str( data[1] ) )
 print "write success : %s" % new_file_path
-print str(time.time()-s_time)
 wb2.save( new_file_path )
